2337add4e01b3f3be51dd2e61833fcc06f8bd23b
Lev Aminov <leoderoko@gmail.com>
2018-02-17 12:07:17 +0500

    Add test profile

--- profiles/test_keikogi.sh
+++ profiles/test_keikogi.sh
@@ -2,6 +2,7 @@
 
 deploy() {
     SSH_CMD="ssh -p 3389 -o UserKnownHostsFile=/dev/null -o LogLevel=quiet -o StrictHostKeyChecking=no $USER@keikogi.ru"
+    SCP_CMD="scp -P 3390 -o UserKnownHostsFile=/dev/null -o LogLevel=quiet -o StrictHostKeyChecking=no"
     $(echo $SSH_CMD) whoami
     $(echo $SSH_CMD) hostname
 }
